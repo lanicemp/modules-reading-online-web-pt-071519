ce60784419417ac0d20b8f768bb0a0f7e4d32cee
LaNice Powell <lanicemp@aol.com>
2019-08-18 17:29:11 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,10 +1,11 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 class Kid
+ extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods 
   
-  
- include Dance
- extend MetaDancing
+# include Dance
+ #extend MetaDancing
  
   attr_accessor :name
  
